8cd9a0e5b24a028e8a5c8a945d2992adb8150242
Kanishka Garg <kanishkagarg410@gmail.com>
2026-07-03 18:26:45 +0530

    added lang change in navbar

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
+import "./i18n"; 
 import App from "./App.jsx";
 import { ThemeProvider } from "./context/ThemeContext";
 import { AuthProvider } from "./context/AuthContext";
